21fba44bed9f3642d76f5c4f95c9a4d93e96f1d9
Liam Rundell <rundellse@gmail.com>
2025-06-15 15:30:17 +0100

    Initial Commit

---
+++ src/main/java/com/rundellse/sportytechtest/SportytechtestApplication.java
@@ -0,0 +1,13 @@
+package com.rundellse.sportytechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyTechTestApplication.class, args);
+	}
+
+}
